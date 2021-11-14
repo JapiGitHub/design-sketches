74d794094020758623a3457da4e890f4e45623f2
Japi MacKorkee <janne.korkee@gmail.com>
2021-11-14 23:55:50 +0200

    firebase deploy

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import "./App.css";
 import HoverReveal from "./components/HoverReveal";
 import Salmiakki from "./components/Salmiakki";
 import VinoMenu from "./components/VinoMenu";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6vIcufEDZ1KG98G7YjpWdn5TNfmhqnFg",
+  authDomain: "fast-sketches.firebaseapp.com",
+  projectId: "fast-sketches",
+  storageBucket: "fast-sketches.appspot.com",
+  messagingSenderId: "617760538612",
+  appId: "1:617760538612:web:85acdb2cc5dab98746c3a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
       <VinoMenu />
-      {/* <Salmiakki /> */}
-      {/* <HoverReveal /> */}
+      {/* <Salmiakki />*/}
+      {/*<HoverReveal />*/}
     </div>
   );
 }
--- src/components/VinoMenu.jsx
+++ src/components/VinoMenu.jsx
@@ -22,84 +22,98 @@ export default function VinoMenu() {
   ]);
 
   useEffect(() => {
+    gsap.from(".vino-anim", { opacity: 0, duration: 0.4, stagger: -0.1 });
+    gsap.from(".button-anim", { opacity: 0, duration: 0.4, stagger: 0.2 });
+  }, []);
+
+  useEffect(() => {
     gsap.to(".vino-anim", { y: -220 * chosenMenuItem });
 
     //gsap.to(`.vino-anim`, { opacity: 0.15 });
     //gsap.to(`#menu-item-${chosenMenuItem}`, { opacity: 1  });
 
-    gsap.to(".vinomenu-title", {
-      transform: `translateX(-${30 * chosenMenuItem}vw)`,
+    gsap.to(".item-text-container", {
+      transform: `translateX(-${32 * chosenMenuItem}vw)`,
     });
   }, [chosenMenuItem]);
 
   return (
     <div>
       <div className="vinomenu-container fullscreen">
-        <article className="vinomenu-title">
-          {selectedTextTitle.map((title, idx) => {
-            return (
-              <>
-                <article
-                  className={`item-text-container ${
-                    chosenMenuItem === idx ? "chosen-text" : null
-                  }`}
-                >
-                  <aside className="title-small-number">
-                    _{(idx + 1).toString().padStart(2, "0")}
-                  </aside>
-                  <div className="item-title">{title.toUpperCase()}</div>
-                  <div className="item-text">{selectedText[idx]}</div>
-                </article>
-              </>
-            );
-          })}
-        </article>
-
-        <div className="menu-items-container">
+        {" "}
+        <div className="gradient-container fullscreen">
           <aside id="vino-upper-gradient"></aside>
+        </div>
+        <div className="title-container fullscreen">
+          <article className="vinomenu-title">
+            {selectedTextTitle.map((title, idx) => {
+              return (
+                <>
+                  <article
+                    className={`item-text-container anim ${
+                      chosenMenuItem === idx ? "chosen-text" : null
+                    }`}
+                  >
+                    <aside className="title-small-number">
+                      _{(idx + 1).toString().padStart(2, "0")}
+                    </aside>
+                    <div className="item-title">{title.toUpperCase()}</div>
+                    <div className="item-text">{selectedText[idx]}</div>
+                  </article>
+                </>
+              );
+            })}
+          </article>
+        </div>
+        <div className="menu-items-container">
           {selectedTextTitle.map((linkPic, idx) => {
             return (
               <>
-                <hr className="vino-anim"></hr>
-                <article
-                  className="two menu-item vino-anim"
-                  style={{
-                    opacity: 0.3 - Math.abs(chosenMenuItem - idx) * 0.1,
-                  }}
-                  style={{
-                    opacity:
-                      chosenMenuItem === idx
-                        ? 1
-                        : 0.2 - Math.abs(chosenMenuItem - idx) * 0.04,
-                  }}
-                  id={`menu-item-${idx + 1}`}
-                >
-                  <aside
-                    className={`vinomenu-pic-number ${
-                      chosenMenuItem === idx ? "hide-pic-number" : null
-                    }`}
+                <hr className="vino-anim anim"></hr>
+
+                <div className="number-pic-container">
+                  <article
+                    className="menu-item vino-anim anim"
+                    style={{
+                      opacity: 0.3 - Math.abs(chosenMenuItem - idx) * 0.1,
+                    }}
+                    style={{
+                      opacity:
+                        chosenMenuItem === idx
+                          ? 1
+                          : 0.2 - Math.abs(chosenMenuItem - idx) * 0.04,
+                    }}
+                    id={`menu-item-${idx + 1}`}
                   >
+                    <aside
+                      className={`vinomenu-pic-number ${
+                        chosenMenuItem === idx ? "hide-pic-number" : null
+                      }`}
+                    >
+                      _{(idx + 1).toString().padStart(2, "0")}
+                    </aside>
+
+                    <video
+                      src="./drops.small.mp4"
+                      muted
+                      loop="true"
+                      autoPlay
+                      preload
+                    ></video>
+                  </article>
+                  <aside className="item-number vino-anim anim">
                     _{(idx + 1).toString().padStart(2, "0")}
                   </aside>
-
-                  <video
-                    src="./drops.mp4"
-                    muted
-                    loop="true"
-                    autoPlay
-                    preload
-                  ></video>
-                </article>
+                </div>
               </>
             );
           })}
         </div>
-
         <section className="lowbar-buttons-container">
           {selectedTextTitle.map((button, idx) => {
             return (
               <div
-                className="lowbar-button"
+                className="lowbar-button button-anim"
                 onMouseEnter={(e) => {
                   setChosenMenuItem(idx);
                 }}
